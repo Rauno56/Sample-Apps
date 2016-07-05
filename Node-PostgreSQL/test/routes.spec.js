2a414d707c0567386faaa884320d93505ac717bb
Ankush <ankushm@zelo.in>
2016-07-05 12:21:54 +0530

    Modifying test cases for negative tests

--- Node-PostgreSQL/test/routes.spec.js
+++ Node-PostgreSQL/test/routes.spec.js
@@ -7,38 +7,45 @@ var knex = require('../db/knex');
 var should = chai.should();
 chai.use(chaiHttp);
 describe('API Routes', function() {
-    // beforeEach(function(done) {});
-    // afterEach(function(done) {});
     describe('GET /api/jobbatical/topActiveUsers?pageNumber={pageNumber}', function() {
-        it('should return all top active users', function(done) {
+        it('should return error object for pageNumber Undefined', function(done) {
+            chai.request(server).get('/api/jobbatical/topActiveUsers').end(function(err, res, body) {
+                res.should.have.status(401);
+                done();
+            });
+        });
+        it('should return error object for pageNumber invalid', function(done) {
+            chai.request(server).get('/api/jobbatical/topActiveUsers?pageNumber=-1').end(function(err, res) {
+                res.should.have.status(402);
+                done();
+            });
+        });
+        it('should return all top active users of past week', function(done) {
             chai.request(server).get('/api/jobbatical/topActiveUsers?pageNumber=1').end(function(err, res) {
                 res.should.have.status(200);
                 res.error.should.be.equal(false);
-                res.should.be.json; // jshint ignore:line
-                res.text.message.should.be.a('string');
-                // res.body.should.be.a('array');
+                res.should.be.json;
                 done();
             });
         });
     });
     describe('GET /api/jobbatical/users?id={user.id}', function() {
+        it('should return error for user id undefined', function(done) {
+            chai.request(server).get('/api/jobbatical/users').end(function(err, res) {
+                res.should.have.status(401);
+                done();
+            });
+        });
+        it('should return error object for user id invalid', function(done) {
+            chai.request(server).get('/api/jobbatical/users?id=-2').end(function(err, res) {
+                res.should.have.status(402);
+                done();
+            });
+        });
         it('should return details for a user', function(done) {
             chai.request(server).get('/api/jobbatical/users?id=2').end(function(err, res) {
                 res.should.have.status(200);
-                res.should.be.json; // jshint ignore:line
-                // res.body.should.be.json; // jshint ignore:line
-                // res.body.should.have.property('id');
-                // res.body.should.have.property('name');
-                // res.body.should.have.property('created_at');
-                // res.body.should.have.property('companies');
-                // res.body.should.have.property('createdListings');
-                // res.body.should.have.property('applications');
-                // res.body.id.should.be.a('integer');
-                // res.body.name.should.be.a('string');
-                // res.body.created_at.should.be.a('date');
-                // res.body.companies.should.be.a('array');
-                // res.body.createdListings.should.be.a('array');
-                // res.body.applications.should.be.a('array');
+                res.should.be.json;
                 done();
             });
         });
